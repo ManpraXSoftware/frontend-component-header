4b0107604b73aafba36d660f21a0f39c3e874944
Ajamal Husain <dpython331@gmail.com>
2024-10-04 13:39:41 +0530

    added header for subodha

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,162 +1,157 @@
-import React, { useContext } from 'react';
-import Responsive from 'react-responsive';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
-import { AppContext } from '@edx/frontend-platform/react';
-import {
-  APP_CONFIG_INITIALIZED,
-  ensureConfig,
-  mergeConfig,
-  getConfig,
-  subscribe,
-} from '@edx/frontend-platform';
+import React, { Component } from 'react';
+import HeaderLogo from "./header-logo";
+import accessibilityIcon from './assets/accessibilityIcon.png';
+import axios from 'axios';
 
-import PropTypes from 'prop-types';
-import DesktopHeader from './DesktopHeader';
-import MobileHeader from './MobileHeader';
+class Header extends Component {
+  componentDidMount() {
+    const jf = document.createElement('script');
+    jf.src = process.env.LMS_BASE_URL + '/static/js/toolkitjs/vebarl.js';
+    jf.type = 'text/javascript';
+    jf.id = 'external_js'
+    jf.setAttribute("lms_base_url", process.env.LMS_BASE_URL)
+    const parentDiv = document.getElementById('nett-head');
+    const localizeScript = document.createElement('script');
+    localizeScript.src = "https://global.localizecdn.com/localize.js";
+    const jqueryScript = document.createElement('script');
+    jqueryScript.src = "https://code.jquery.com/jquery-3.7.1.js";
+    jqueryScript.integrity = "sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=";
+    jqueryScript.crossOrigin = "anonymous";
+    const localizeInnerText = document.createElement("script");
+    localizeInnerText.innerText = !function (a) { if (!a.Localize) { a.Localize = {}; for (var e = ["translate", "untranslate", "phrase", "initialize", "translatePage", "setLanguage", "getLanguage", "getSourceLanguage", "detectLanguage", "getAvailableLanguages", "untranslatePage", "bootstrap", "prefetch", "on", "off", "hideWidget", "showWidget"], t = 0; t < e.length; t++)a.Localize[e[t]] = function () { } } }(window);
+    const localizeKey = document.createElement("script");
+    localizeKey.innerText = Localize.initialize({key: 'zKxxnKn5hZxwu',rememberLanguage: true,});
+    const langSelect = document.createElement("select");
+    langSelect.id = "langOptions";
+    langSelect.className = "myLang";
+    langSelect.ariaLabel = "Selected language";
+    parentDiv.append(jf);
+    parentDiv.append(jqueryScript);
+    parentDiv.append(localizeInnerText);
+    parentDiv.append(localizeKey);
+    parentDiv.append(langSelect);
+    const bodyDiv = document.body;
+    bodyDiv.append(localizeScript)
+    let selectTag = document.getElementById("langOptions");
+    Localize.getAvailableLanguages((error, data) => {
+      data.map((e, i) => {
+      var lang_name = e.name;
+      if (e.code == "hi-IN" || e.code == "hi") {
+        lang_name = e.name + "(Hindi)";
+      } else if (e.code == "kn") {
+        lang_name = e.name + "(Kannada)";
+      } else if (e.code == "bn") {
+        lang_name = e.name + "(Bangali)"
+      } 
+      else if (e.code == "en") {
+        lang_name = e.name + "(English)"
+      } else if (e.code == "ta-IN") {
+        lang_name = e.name + "(Tamil (India))"
+      } else if (e.code == "or") {
+        lang_name = e.name + "(Odia)"
+      } else if (e.code == "ml-IN" || e.code == "ml") {
+        lang_name = e.name + "(Malayalam)"
+      }
+      var newOption = new Option(lang_name, e.code)
+      selectTag.append(newOption)
+      })
+    });
+    axios.get(process.env.LMS_BASE_URL + '/mx-user-info/get_user_profile').then((res) => {
+      for (let i = 0; i < res.data.dark_languages.length; i++) {
+        var code = res.data.dark_languages[i][0]
+        var name = res.data.dark_languages[i][1]
+        if (code != 'en') {
+          if (code == "hi-IN" || code == "hi") {
+            name = name + "(Hindi)";
+          } else if (code == "kn") {
+            name = name + "(Kannada)";
+          } else if (code == "bn") {
+            name = name + "(Bangali)"
+          } else if (code == "en") {
+            name = name + "(English)"
+          } else if (code == "ta-IN") {
+            name = name + "(Tamil (India))"
+          } else if (code == "or") {
+            name = name + "(Odia)"
+          } else if (code == "ml-IN" || code == "ml") {
+            name = name + "(Malayalam)"
+          }
+          var option = new Option(name, code)
+          selectTag.append(option)
+        }
+      }
+    })
 
-import messages from './Header.messages';
-
-ensureConfig([
-  'LMS_BASE_URL',
-  'LOGOUT_URL',
-  'LOGIN_URL',
-  'SITE_NAME',
-  'LOGO_URL',
-  'ORDER_HISTORY_URL',
-], 'Header component');
-
-subscribe(APP_CONFIG_INITIALIZED, () => {
-  mergeConfig({
-    AUTHN_MINIMAL_HEADER: !!process.env.AUTHN_MINIMAL_HEADER,
-  }, 'Header additional config');
-});
-
-/**
- * Header component for the application.
- * Displays a header with the provided main menu, secondary menu, and user menu when the user is authenticated.
- * If any of the props (mainMenuItems, secondaryMenuItems, userMenuItems) are not provided, default
- * items are displayed.
- * For more details on how to use this component, please refer to this document:
- * https://github.com/openedx/frontend-component-header/blob/master/docs/using_custom_header.rst
- *
- * @param {list} mainMenuItems - The list of main menu items to display.
- * See the documentation for the structure of main menu item.
- * @param {list} secondaryMenuItems - The list of secondary menu items to display.
- * See the documentation for the structure of secondary menu item.
- * @param {list} userMenuItems - The list of user menu items to display.
- * See the documentation for the structure of user menu item.
- */
-const Header = ({
-  intl, mainMenuItems, secondaryMenuItems, userMenuItems,
-}) => {
-  const { authenticatedUser, config } = useContext(AppContext);
-
-  const defaultMainMenu = [
-    {
-      type: 'item',
-      href: `${config.LMS_BASE_URL}/dashboard`,
-      content: intl.formatMessage(messages['header.links.courses']),
-    },
-  ];
-  const defaultUserMenu = authenticatedUser === null ? [] : [{
-    heading: '',
-    items: [
-      {
-        type: 'item',
-        href: `${config.LMS_BASE_URL}/dashboard`,
-        content: intl.formatMessage(messages['header.user.menu.dashboard']),
-      },
-      {
-        type: 'item',
-        href: `${config.ACCOUNT_PROFILE_URL}/u/${authenticatedUser.username}`,
-        content: intl.formatMessage(messages['header.user.menu.profile']),
-      },
-      {
-        type: 'item',
-        href: config.ACCOUNT_SETTINGS_URL,
-        content: intl.formatMessage(messages['header.user.menu.account.settings']),
-      },
-      // Users should only see Order History if have a ORDER_HISTORY_URL define in the environment.
-      ...(config.ORDER_HISTORY_URL ? [{
-        type: 'item',
-        href: config.ORDER_HISTORY_URL,
-        content: intl.formatMessage(messages['header.user.menu.order.history']),
-      }] : []),
-      {
-        type: 'item',
-        href: config.LOGOUT_URL,
-        content: intl.formatMessage(messages['header.user.menu.logout']),
-      },
-    ],
-  }];
-
-  const mainMenu = mainMenuItems || defaultMainMenu;
-  const secondaryMenu = secondaryMenuItems || [];
-  const userMenu = authenticatedUser === null ? [] : userMenuItems || defaultUserMenu;
-
-  const loggedOutItems = [
-    {
-      type: 'item',
-      href: config.LOGIN_URL,
-      content: intl.formatMessage(messages['header.user.menu.login']),
-    },
-    {
-      type: 'item',
-      href: `${config.LMS_BASE_URL}/register`,
-      content: intl.formatMessage(messages['header.user.menu.register']),
-    },
-  ];
-
-  const props = {
-    logo: config.LOGO_URL,
-    logoAltText: config.SITE_NAME,
-    logoDestination: `${config.LMS_BASE_URL}/dashboard`,
-    loggedIn: authenticatedUser !== null,
-    username: authenticatedUser !== null ? authenticatedUser.username : null,
-    avatar: authenticatedUser !== null ? authenticatedUser.avatar : null,
-    mainMenu: getConfig().AUTHN_MINIMAL_HEADER ? [] : mainMenu,
-    secondaryMenu: getConfig().AUTHN_MINIMAL_HEADER ? [] : secondaryMenu,
-    userMenu: getConfig().AUTHN_MINIMAL_HEADER ? [] : userMenu,
-    loggedOutItems: getConfig().AUTHN_MINIMAL_HEADER ? [] : loggedOutItems,
-  };
-
-  return (
-    <>
-      <Responsive maxWidth={769}>
-        <MobileHeader {...props} />
-      </Responsive>
-      <Responsive minWidth={769}>
-        <DesktopHeader {...props} />
-      </Responsive>
+  }
+  
+  render() {
+  
+    return (<>
+      <div className="uai userway_dark"  id="userwayAccessibilityIcon" aria-label="accessibility menu" role="button" tabIndex={1} >
+        <img alt="Accessibility Widget" src={accessibilityIcon} className="ui_w" width="35" height="35" />
+      </div>
+      <header className="global-header" id="nett-head">
+        <div className="main-header">
+          <HeaderLogo />
+          <div className="hamburger-menu" role="button" aria-label="Options Menu" aria-expanded={false} aria-controls="mobile-menu" tabIndex={0}>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+          </div>
+          <div className="nav-links">
+            <div className="main">
+              <nav className="navbar navbar-expand-sm   navbar-light bg-light head_menu" aria-label="primary">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                  <ul className="navbar-nav mr-auto mt-2 mt-lg-0 menu_list">
+                    <li className="nav-item">
+                      <a className={window.location.href.includes('/explore-courses/') ? 'active tab-nav-link' : 'tab-nav-link'} href="/explore-courses/" accessKey="c"
+                        aria-current="page">Explore Courses</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className={window.location.href.includes('dashboard/programs/') ? 'active tab-nav-link' : 'tab-nav-link'} href={process.env.LMS_BASE_URL + 'dashboard/programs/'} accessKey="s"
+                        aria-current="page">
+                        Dashboard
+                      </a>
+                    </li>
+                  </ul>
+                  <div className="search_box">
+                    <form className='headerSearchForm' onSubmit={(e) => { e.preventDefault() }}>
+                      <div className="form-group" role='search'>
+                        <input type="text" id="heardeSearch" name="Search for topic of interest" placeholder="Search for topic of interest" className="enter" onChange={(e) => { }} />
+                        <input type="submit" value="" className="submit" aria-label="Search" />
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </nav>
+            </div>
+            <div className="secondary" onClick={(e) => { document.getElementById("user-menu").classList.toggle("hidden") }}>
+              <div className="nav-item hidden-mobile user_custom_login toggle-user-dropdown" aria-label="User Account Options" aria-expanded="false" tabIndex={0} aria-controls="user-menu">
+                <span className="menu-title" aria-hidden="true">
+                  <img className="user-image-frame" id="profileimageid" src="" alt="" />
+                  <span className="sr-only" aria-disabled="true">Dashboard for:</span>
+                  <span className="username" aria-disabled="true" id="header-username"></span>
+                </span>
+              </div>
+              <div className="nav-item hidden-mobile nav-item-dropdown">
+                <div className="toggle-user-dropdown" role="button" aria-label="Options Menu" aria-expanded="false" tabIndex={-1} aria-controls="user-menu">
+                  <span className="fa fa-caret-down" aria-hidden="true"></span>
+                </div>
+                <div className="dropdown-user-menu hidden" aria-label="More Options" role="menu" id="user-menu" tabIndex={-1}>
+                  <div className="mobile-nav-item dropdown-item dropdown-nav-item" id="dashboard-navbar"><a href={process.env.LMS_BASE_URL + 'dashboard/programs/'} role="menuitem">Dashboard</a></div>
+                  <div className="mobile-nav-item dropdown-item dropdown-nav-item" ><a id="user-profiler-redirect" href="" role="menuitem">Profile</a></div>
+                  <div className="mobile-nav-item dropdown-item dropdown-nav-item"><a href={process.env.LMS_BASE_URL + 'account/settings'} role="menuitem">Account</a></div>
+                  <div className="mobile-nav-item dropdown-item dropdown-nav-item"><a href={process.env.LMS_BASE_URL + 'logout'} role="menuitem">Sign Out</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </>
-  );
-};
+    );
+  }
+}
 
-Header.defaultProps = {
-  mainMenuItems: null,
-  secondaryMenuItems: null,
-  userMenuItems: null,
-};
-
-Header.propTypes = {
-  intl: intlShape.isRequired,
-  mainMenuItems: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.array,
-  ]),
-  secondaryMenuItems: PropTypes.oneOfType([
-    PropTypes.node,
-    PropTypes.array,
-  ]),
-  userMenuItems: PropTypes.arrayOf(PropTypes.shape({
-    heading: PropTypes.string,
-    items: PropTypes.arrayOf(PropTypes.shape({
-      type: PropTypes.oneOf(['item', 'menu']),
-      href: PropTypes.string,
-      content: PropTypes.string,
-      isActive: PropTypes.bool,
-    })),
-  })),
-};
-
-export default injectIntl(Header);
+export default Header;
