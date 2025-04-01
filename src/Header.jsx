4747cae8e9f19c3ac513beb4fd531afb5fcf3d8c
Om <om.kumar@manprax.com>
2025-04-01 16:40:03 +0530

    clear search box after redirecting to search page

--- src/Header.jsx
+++ src/Header.jsx
@@ -489,6 +489,7 @@ class Header extends Component {
       if (searchData != ""){
         let url =  getConfig().EXPLORE_COURSE_URL[0] + `/search?text=${searchData}`;
         window.location = url;  
+        $('.enter').val('');
     }
 
   }
