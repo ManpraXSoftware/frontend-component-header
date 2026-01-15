dc572c79a61dfd2e4786419996e58a2e98ff88b2
Om <om.kumar@manprax.com>
2026-01-15 21:12:47 +0530

    added aria-disable in btn

--- src/AudioSearch.js
+++ src/AudioSearch.js
@@ -1107,6 +1107,7 @@ triggerAnnouncementSequence = () => {
                       // aria-label={this.getSpeakAriaLabel()}
                       aria-label="speak"
                       aria-describedby="speak-desc"
+                      aria-disabled={this.state.isListening || this.isStartingRef.current || this.isStoppingRef.current || !this.state.canRespeak ? 'true' : 'false'}
 
                     >
                       Speak
@@ -1130,6 +1131,8 @@ triggerAnnouncementSequence = () => {
                       // aria-label={this.getStopAriaLabel()}
                       aria-label="Stop"
                       aria-describedby="stop-desc"
+                      aria-disabled={!this.state.isListening || this.isStoppingRef.current ? 'true' : 'false'}
+
                     >
                       <FontAwesomeIcon icon={faStop} /> Stop
                     </button>
@@ -1164,6 +1167,8 @@ triggerAnnouncementSequence = () => {
                       // aria-label={this.getSearchAriaLabel()}
                       aria-label="Search"
                       aria-describedby="search-desc"
+                      aria-disabled={!this.state.canSearch? 'true' : 'false'}
+
                     >
                       Search
                     </button>
